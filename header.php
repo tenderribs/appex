451da46fbcb81a68000dd8045b3a37a676d8d6b7
tenderribs <dc3dude@gmail.com>
2018-05-04 19:56:08 +0200

    changed the navbars color

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
             <!--Navbar-->
             <nav class="navbar is-fixed-top" style="background-color: #448afc" role="navigation" aria-label="main navigation">
                     <div class="navbar-brand">
-                      <a class="navbar-item has-text-black-bis" style="background-color: #89d2ff" class="appex-navbar" href="../index.php">
+                      <a class="navbar-item has-text-black-bis" style="background-color: #2175ff" class="appex-navbar" href="../index.php">
 
                         <img src="images\logo.gif" alt="Applied Experience" width="75" height="32">
 
@@ -22,10 +22,5 @@
                         <a class="navbar-item has-text-black-ter" href="index.php?page=about">
                             About
                         </a>
-                      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                      </a>
                     </div>
             </nav>
