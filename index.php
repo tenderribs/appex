af24d3a839df3089ff6d07d31cbd677ce8e41098
MaJeD <majed@prine.ch>
2018-05-06 11:36:38 +0200

    fixed indents and added missing class and some other cool and tricky fixes that you have never seen before, be sure to checkout the changes

--- index.php
+++ index.php
@@ -1,51 +1,33 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="favicon.ico" />
-        <link rel="stylesheet" type="text/css" href="css\main.css">
-        <title>AppEx</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css">
-        <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-        <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="images/favicon/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
-        <link rel="manifest" href="images/favicon/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
+        <?php require_once('metaData.php');   ?>
     </head>
     <body>
-      <?php
-            //navbar
-          require_once('header.php');
-          $page = $_GET["page"];
-            //page content
-          if ($page == '') {
-            require_once('pages/home.html');
+        <?php     //navbar
+            require_once('header.php');
+        ?>   
+        <section class="section">
+            <div class="container">
+            <?php
+                
+                $page = isset($_GET["page"]) ? $_GET["page"] : null;
+                    //page content
+                if ($page == '') {
+                    require_once('pages/home.html');
 
-          } else if ($page == 'features') {
-            require_once('pages/features.html');
+                } else if ($page == 'features') {
+                    require_once('pages/features.html');
 
-          } else if ($page == 'system') {
-            require_once('pages/system.html');
+                } else if ($page == 'system') {
+                    require_once('pages/system.html');
 
-          } else if ($page == 'blog') {
-            require_once('pages/blog.html');
-          }
-      ?>
-
+                } else if ($page == 'blog') {
+                    require_once('pages/blog.html');
+                }
+            ?>
+            </div>
+        </section>
 
 
     </body>
